99000bc63f8365af9dd29ee483a0fd60706f1e7d
Ternder001 <nicholas.akura@gmail.com>
2024-02-26 21:04:11 +0100

    first commit

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -2,10 +2,12 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
 contract NFTMarketplace is ERC721, Ownable {
+    using Counters for Counters.Counter;
     uint counter = 1;
     counter private _tokenIdTracker;
 
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,18 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [owner] =  await ethers.getSigners();
 
-  const lockedAmount = ethers.parseEther("0.001");
+  const NFT = await ethers.deployContract("NFT", [owner]);
+  await NFT.waitForDeployment();
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  const Marketplace = await ethers.deployContract("NFTMarketplace.sol", [
+    owner,
+  ]);
+  await Marketplace.waitForDeployment();
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log(`NFT deployed to ${NFT.target}`);
+  console.log(`NFT Marketplace deployed to ${Marketplace.target}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
